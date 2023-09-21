8b571124578f81b60e867f7667b255fe247dd6b0
Susumu Noda <susumunoda@gmail.com>
2023-09-21 11:39:52 -0700

    Fix tile spacing issue
    When calculating tile size, some rounding happens because pixels are
    discrete values. Because of this, providing a hard-coded spacing between
    tiles can cause the final column to have a slightly smaller width than
    the rest of the columns.
    
    To fix this, configure the rows and columns to render the tiles with
    space between entries, rather than hard-coding the spacing value.

--- shared/src/commonMain/kotlin/com/susumunoda/wordgame/App.kt
+++ shared/src/commonMain/kotlin/com/susumunoda/wordgame/App.kt
@@ -1,19 +1,13 @@
 package com.susumunoda.wordgame
 
-import com.susumunoda.wordgame.TileType.BL
-import com.susumunoda.wordgame.TileType.DL
-import com.susumunoda.wordgame.TileType.DW
-import com.susumunoda.wordgame.TileType.ST
-import com.susumunoda.wordgame.TileType.TL
-import com.susumunoda.wordgame.TileType.TW
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.BoxWithConstraints
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
@@ -29,6 +23,12 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.susumunoda.wordgame.TileType.BL
+import com.susumunoda.wordgame.TileType.DL
+import com.susumunoda.wordgame.TileType.DW
+import com.susumunoda.wordgame.TileType.ST
+import com.susumunoda.wordgame.TileType.TL
+import com.susumunoda.wordgame.TileType.TW
 
 private enum class TileType(val color: Color) {
     TW(Color(255, 74, 195)), // Triple word
@@ -57,36 +57,44 @@ private val GRID = arrayOf(
     arrayOf(TW, BL, BL, DL, BL, BL, BL, TW, BL, BL, BL, DL, BL, BL, TW), ////////
 )
 
-private val GRID_PADDING = 8.dp
 private val TILE_SPACING = 2.dp
 private val TILE_FONT_SIZE = 8.sp
 private val TILE_ROUNDING = 4.dp
 
-@OptIn(ExperimentalStdlibApi::class)
 @Composable
 fun App() {
     MaterialTheme {
-        BoxWithConstraints(Modifier.fillMaxSize().padding(GRID_PADDING)) {
+        BoxWithConstraints {
             val tileSize = (maxWidth - (TILE_SPACING * (GRID.size - 1))) / GRID.size
-            Column(verticalArrangement = Arrangement.spacedBy(TILE_SPACING)) {
-                GRID.forEachIndexed { _, row ->
-                    Row(horizontalArrangement = Arrangement.spacedBy(TILE_SPACING)) {
-                        row.forEachIndexed { _, tileType ->
-                            Box(
-                                modifier = Modifier
-                                    .size(tileSize)
-                                    .clip(RoundedCornerShape(TILE_ROUNDING))
-                                    .background(tileType.color),
-                                contentAlignment = Alignment.Center,
-                            ) {
-                                if (tileType == ST) {
-                                    Icon(Icons.Filled.Star, null, tint = Color.White)
-                                } else {
-                                    Text(
-                                        tileType.name,
-                                        fontSize = TILE_FONT_SIZE,
-                                        fontWeight = FontWeight.Bold
-                                    )
+
+            // Assuming we are in portrait mode, the board is a square that is width * width in size
+            Box(Modifier.size(maxWidth)) {
+                Column(
+                    modifier = Modifier.fillMaxHeight(),
+                    verticalArrangement = Arrangement.SpaceBetween
+                ) {
+                    GRID.forEach { row ->
+                        Row(
+                            modifier = Modifier.fillMaxWidth(),
+                            horizontalArrangement = Arrangement.SpaceBetween
+                        ) {
+                            row.forEach { tileType ->
+                                Box(
+                                    modifier = Modifier
+                                        .size(tileSize)
+                                        .clip(RoundedCornerShape(TILE_ROUNDING))
+                                        .background(tileType.color),
+                                    contentAlignment = Alignment.Center
+                                ) {
+                                    if (tileType == ST) {
+                                        Icon(Icons.Filled.Star, null, tint = Color.White)
+                                    } else {
+                                        Text(
+                                            tileType.name,
+                                            fontSize = TILE_FONT_SIZE,
+                                            fontWeight = FontWeight.Bold
+                                        )
+                                    }
                                 }
                             }
                         }
