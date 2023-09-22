6b8fba9bf316d3ea7722431a7e5e8c83e7d7e8ab
Susumu Noda <susumunoda@gmail.com>
2023-09-21 19:46:32 -0700

    Add horizontal padding to game board

--- shared/src/commonMain/kotlin/com/susumunoda/wordgame/ui/GameScreen.kt
+++ shared/src/commonMain/kotlin/com/susumunoda/wordgame/ui/GameScreen.kt
@@ -1,8 +1,14 @@
 package com.susumunoda.wordgame.ui
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import com.susumunoda.wordgame.WordGameState
+
+private val HORIZONTAL_PADDING = PaddingValues(horizontal = 4.dp)
 
 @Composable
 fun GameScreen(state: WordGameState) {
@@ -12,7 +18,7 @@ fun GameScreen(state: WordGameState) {
             playerTwoData = state.playerTwoData,
             currentTurnPlayer = state.currentTurnPlayer
         )
-        GameBoard()
+        GameBoard(Modifier.padding(HORIZONTAL_PADDING))
         PlayerTiles()
     }
 }
